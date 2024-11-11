dc8708063eff0af72eedf070c98c9f102f03e512
WFriver <wrivera@quickapps.mx>
2024-11-11 12:36:30 -0600

    First aplication
    First aplication in react native, counter

---
+++ counter-app/App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import FAB from './components/FAB';
+
+export default function App() {
+
+  const [count, setCount] = useState(10)
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textHuge}>{count}</Text>
+      <FAB label='+1'
+        onLongPress={() => setCount(0)}
+        onPress={() => setCount(count + 1)}
+        position='right' />
+
+      <FAB label='Reset'
+        onPress={() => setCount(0)}
+        position='left' />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100',
+  },
+});
